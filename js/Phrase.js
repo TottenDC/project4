2b4bd9499741cd7bae3b114a11fc394e031e036d
Daniel Totten <TottenDC@gmail.com>
2018-10-09 10:06:35 -0400

    Created addPhraseToDisplay() method

--- js/Phrase.js
+++ js/Phrase.js
@@ -4,7 +4,20 @@ class Phrase {
   }
 
   addPhraseToDisplay() {
+    const phraseSplit = this.phrase.split(/(\w)/);
+    const phraseLetters = phraseSplit.filter(character = character !== '');
 
+    for (letter of phraseLetters) {
+      const playSpace = document.querySelector('#phrase').firstElementChild;
+      let li = document.createElement('li');
+      li.textContent = letter;
+      if (letter !== ' ') {
+        li.className = `hide letter ${letter}`;
+      } else {
+        li.className = `hide space`;
+      }
+      playSpace.appendChild(li);
+    }
   }
 
   checkLetter() {
@@ -12,6 +25,6 @@ class Phrase {
   }
 
   showMatchedLetter() {
-    
+
   }
 }
