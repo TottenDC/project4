bc36a91ad732155f0e1d4dd731ba7765320c86d7
Daniel Totten <TottenDC@gmail.com>
2018-10-10 14:29:12 -0400

    Created clues and updated the getRandomPhrase() method to reflect them

--- js/Game.js
+++ js/Game.js
@@ -1,13 +1,43 @@
 class Game {
   constructor() {
     this.missed = 0;
-    this.phrases = ['phrase', 'another phrase', 'last phrase'];
+    this.phrases = [
+      ['final fantasy', 'the last of us', 'super mario galaxy'],
+      ['where the red fern grows', 'jurassic park', 'confederacy of dunces'],
+      ['not now ma', 'can you believe that backstabbing slut', 'grab that dough'],
+      ['knit one purl one', 'whittle soap', 'the looming loom in the room']];
     this.phrase = null;
+    this.clue = null;
   }
 
   getRandomPhrase() {
-    const randomNum = Math.floor(Math.random() * this.phrases.length);
-    return this.phrases[randomNum];
+    const index1 = Math.floor(Math.random() * this.phrases.length);
+    function index2(index1) {
+      return Math.floor(Math.random() * game.phrases[index1].length);
+    }
+    if (index1 === 0) {
+      this.clue = 'Video Games';
+      return this.phrases[index1][index2(index1)];
+    } else if (index1 === 1) {
+      this.clue = 'Literature';
+      return this.phrases[index1][index2(index1)];
+    } else if (index1 === 2) {
+      this.clue = 'Golden Girls Quotes';
+      return this.phrases[index1][index2(index1)];
+    } else {
+      this.clue = 'Crafts';
+      return this.phrases[index1][index2(index1)];
+    }
+  }
+
+  addClueToDisplay() {
+    const header = document.querySelector('#banner');
+    if (header.firstElementChild.nextElementSibling) {
+      header.removeChild(header.firstElementChild.nextElementSibling);
+    }
+    const clueHeader = document.createElement('h3');
+    clueHeader.innerText = `Clue: ${this.clue}`;
+    header.appendChild(clueHeader);
   }
 
   handleInteraction(event) {
@@ -33,6 +63,7 @@ class Game {
 
   startGame() {
     this.phrase = new Phrase(this.getRandomPhrase());
+    this.addClueToDisplay();
     this.phrase.addPhraseToDisplay();
   }
 }
