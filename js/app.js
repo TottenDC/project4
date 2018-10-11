2a1df1d08887681c722a995101dbccc11832dfc2
Daniel Totten <TottenDC@gmail.com>
2018-10-11 13:02:57 -0400

    Added code comments to app.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,33 @@
 let game = null;
 
+/**
+* Hides the start screen display
+*/
 function resetDisplay() {
   document.querySelector('#overlay').style.display='none';
 }
 
+/**
+* When a keyboard button is clicked, sets disabled attribute before handing the event to the game object to check the letter
+* @param {Object} event The event object triggered when a keyboard button is clicked
+*/
 function markButton(event) {
   event.target.setAttribute('disabled', '');
   game.handleInteraction(event);
 }
 
+/**
+* Sets event handler on start button to set up a game object when clicked
+*/
 document.querySelector('#btn__reset').addEventListener('click', () => {
   resetDisplay();
   game = new Game();
   game.startGame();
 });
 
+/**
+* Sets event handler on all keyboard buttons to launch game logic
+*/
 document.querySelector('#qwerty').addEventListener('click', (event) => {
   markButton(event);
 });
