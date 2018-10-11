7fdcf1bbc0d8bd71b1648e637ba25265872f0ef5
Daniel Totten <TottenDC@gmail.com>
2018-10-11 12:25:28 -0400

    Added code comments to Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -3,12 +3,19 @@ class Phrase {
     this.phrase = this.generatePhraseArray(phrase);
   }
 
+  /**
+  * Creates an array of a single phrase
+  * @param {String} phrase - a single phrase to use for the game
+  * @return {Array} An array containing the split string without empty string items
+  */
   generatePhraseArray(phrase) {
     return phrase.split(/(\w)/).filter(character => character !== '');
   }
 
+  /**
+  * Creates li elements from phrase array with respective classes to initialize the game
+  */
   addPhraseToDisplay() {
-
     for (let letter of this.phrase) {
       const playSpace = document.querySelector('#phrase').firstElementChild;
       let li = document.createElement('li');
@@ -22,6 +29,11 @@ class Phrase {
     }
   }
 
+  /**
+  * Assesses whether the keyboard button clicked matches a letter in the phrase array
+  * @param {Object}  event The event object triggered when the keyboard button is clicked
+  *@return {Boolean} Returns whether the letter is present in the phrase array
+  */
   checkLetter(event) {
     let guess = event.target.innerText;
     for (let letter of this.phrase) {
@@ -32,6 +44,10 @@ class Phrase {
     return false;
   }
 
+  /**
+  * Changes the class of the phrase list items to make the hidden letter visible
+  * @param {Object} event The event object triggered when the keyboard button is clicked
+  */
   showMatchedLetter(event) {
     const phraseList = document.querySelector('#phrase').firstElementChild.children;
     let guess = event.target.innerText;
